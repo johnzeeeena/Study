3793701932c0b58d77190e0ef53acff24ee30064
Ievgenii Zinchenko <ievgenii.zinchenko@stryker.com>
2020-10-07 19:23:56 +0300

    code change3

--- src/New1.java
+++ src/New1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class New1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your username: ");
+        System.out.print("Enter your username: ");
         String str = scanner.next();
         System.out.println("Hello, " + str + "!");
     }
