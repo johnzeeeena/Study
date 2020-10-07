60fce9b4fccdbf7b9e16eee18d29afb872d21296
Ievgenii Zinchenko <ievgenii.zinchenko@stryker.com>
2020-10-07 14:42:48 +0300

    code change

--- src/Continue.java
+++ src/Continue.java
@@ -1,8 +1,10 @@
 public class Continue {
     public static void main(String[] args) {
         for (int i = 0; i <=100 ; i++) {
-            if (i % 2 == 0) continue;
+            if (i % 2 == 0) {
+            } else {
                 System.out.println(i);
+            }
         }
     }
 }
