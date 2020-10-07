29b55031a1ab3eb1f988b9ad878990aae65c0416
Ievgenii Zinchenko <ievgenii.zinchenko@stryker.com>
2020-10-07 14:57:21 +0300

    code change3

---
+++ src/ThreeDigitNumbers3.java
@@ -0,0 +1,18 @@
+public class ThreeDigitNumbers3 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == i) {
+                    continue;
+                }
+                for (int k = 0; k < 10; k++) {
+                    if (k == j || k == i) {
+                        continue;
+                    }
+                    System.out.println("" + i + j + k);
+                }
+            }
+        }
+    }
+}
