392f57f69332394c56437e0a92ae10690bf1eb72
Ievgenii Zinchenko <ievgenii.zinchenko@stryker.com>
2020-10-07 14:51:09 +0300

    code change

--- src/ThreeDigitNumbers4.java
+++ src/ThreeDigitNumbers4.java
@@ -1,11 +1,10 @@
 public class ThreeDigitNumbers4 {
     public static void main(String[] args) {
         System.out.println("3х-значные числа, в десятичной записи которых нет одинаковых цифр: ");
-        int a = 0, b = 0, c = 0;
         for (int j = 100; j < 1000; j++) {
-            a = j / 10 / 10;
-            b = j / 10 % 10;
-            c = j % 100 % 10;
+           int a = j / 10 / 10;
+           int b = j / 10 % 10;
+           int c = j % 100 % 10;
             if (a != b && a != c && b != c)
                 System.out.println("" + j);
         }
